92029ee342ddea1fb96ca5c4db66f467e545e3ae
Z.Y <441669631@qq.com>
2018-04-13 12:35:30 +0800

    u

---
+++ 4.8/test.m
@@ -0,0 +1,29 @@
+sym 函数
+syms 函数
+compose 函数
+subs 函数
+eval 函数
+
+diff 函数 求导
+int 积分
+
+simplify
+simple
+combine
+factor
+pretty 化简函数
+
+solve 方程求解
+
+dsolve 微分方程求解
+
+taylor
+taylortool
+
+f = sin(x^2)*cos(y^2);
+Latex(f)
+
+matlabFunction
+
+arrayfun
+
